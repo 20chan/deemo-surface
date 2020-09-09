b13b74cc821bed3b6fb1752464c0dd81161b605e
20chan <2@0chan.dev>
2020-09-09 22:06:22 +0900

    mouse hook demo

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿using System;
+using System;
+using System.Threading;
+using Hook;
 
 namespace deemo_surface {
     class Program {
+        [STAThread]
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var waiter = new ManualResetEvent(false);
+            Console.CancelKeyPress += (o, e) => {
+                e.Cancel = true;
+                waiter.Set();
+            };
+
+            RegisterEvents();
+
+            MouseHook.HookStart();
+            waiter.WaitOne();
+            MouseHook.HookEnd();
+        }
+
+        static void RegisterEvents() {
+            MouseHook.MouseDown += (ty, x, y) => {
+                Console.WriteLine($"{ty}, {x}, {y}");
+                return true;
+            };
         }
     }
 }
